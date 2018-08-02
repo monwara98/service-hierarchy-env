9243ffae97eaab36bdc2611aebdc1a2e629e9cb7
Monwara Bibi <monwara.bibi@kcl.ac.uk>
2018-08-02 13:13:32 +0000

    added the table names with the services

--- service-hierarchy/service-hierarchy/views/index.py
+++ service-hierarchy/service-hierarchy/views/index.py
@@ -5,6 +5,8 @@ import psycopg2 as p
 class ReusableForm(Form):
     name = TextField('', validators=[validators.required()])
     
+    
+    
 try:
     bp = Blueprint(__name__, __name__, template_folder='templates')
 except Exception as e:
@@ -18,10 +20,8 @@ else:
         if request.method == 'POST':
             name=request.form['name']
      
-            if form.validate():
-                # Save the comment here.
-                flash("hello")
-                flash(lookup(name,searchingDictionary()))
+            if form.validate(): # save the comment here
+                flash(searchingDictionary(name))
             else:
                 flash('All the form fields are required. ')
      
@@ -32,20 +32,24 @@ else:
             if l[1] == None:
                 list.remove(l)
                 
+                
+                
     def searchByValue(d,v):
         listOfKeys = [key  for (key, value) in d.items() if value == v]
         return listOfKeys
+    
+    
     
     def lookup(word,d):
         if word in d:
             k = d[word] 
             return searchByValue(d,k)
-            # return the value of that key
-            # use searchByValue to get a list of all keys associated with that value
         else:
             return "search not found"
-    
-    def searchingDictionary():
+        
+        
+        
+    def searchingDictionary(word):
         try:
             con = p.connect("dbname='odi' user='fmopex_test_ro' host='fmopex.cl19fspdhrve.eu-west-1.rds.amazonaws.com' password = 'admin'")
         except Exception as e:
@@ -53,95 +57,81 @@ else:
         else:
             print("\nconnected to database\n")
         
-            cursor = con.cursor()
-            cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
-            #row = cursor.fetchall() # gets all the table names from the database
-       
-        # have only started off by doing this to a few services, will learn to 
-        # automate this later and add more tables to the list
-            c = con.cursor() 
-        
-            c.execute("select name,masterserviceid from odi_test.map_dotcom")
-            map_dotcom = c.fetchall()
-            cleaningLists(map_dotcom)
-            d1 = dict(map_dotcom)
-        
-            c.execute("select service,masterserviceid from odi_test.map_dataleaks")
-            map_dataleaks = c.fetchall()
-            cleaningLists(map_dataleaks)
-            d2 = dict(map_dataleaks)
-        
-            c.execute("select service,masterserviceid from odi_test.map_servicenow")
-            map_servicenow = c.fetchall()
-            cleaningLists(map_servicenow)
-            d3 = dict(map_servicenow)
-        
-            c.execute("select service,masterserviceid from odi_test.map_isrisk")
-            map_isrisk = c.fetchall()
-            cleaningLists(map_isrisk)
-            d4 = dict(map_isrisk)
-        
-            c.execute("select service,masterserviceid from odi_test.map_gdpr")
-            map_gdpr = c.fetchall()
-            cleaningLists(map_gdpr)
-            d5 = dict(map_gdpr)
-        
-            c.execute("select service,masterserviceid from odi_test.map_pentest")
-            map_pentest = c.fetchall()
-            cleaningLists(map_pentest)
-            d6 = dict(map_pentest)
-        
-            c.execute("select service,masterserviceid from odi_test.map_remoteconnectivity")
-            map_remoteconnectivity = c.fetchall()
-            cleaningLists(map_remoteconnectivity)
-            d7 = dict(map_remoteconnectivity)
-        
-            c.execute("select service,masterserviceid from odi_test.map_bcp")
-            map_bcp = c.fetchall()
-            cleaningLists(map_bcp)
-            d8 = dict(map_bcp)
-        
-            c.execute("select service,masterserviceid from odi_test.map_pas")
-            map_pas = c.fetchall()
-            cleaningLists(map_pas)
-            d9 = dict(map_pas)
-        
-            c.execute("select * from odi_test.control")
-            control = c.fetchall()
-            cleaningLists(control)
-            d10 = dict(control)
-        
-            c.execute("select service,batch from odi_test.evbbatchmetrics")
-            evbbatchmetrics = c.fetchall()
-            # they've compressed this all into 1 as they all have the same name
-            # sort this out later
-            cleaningLists(evbbatchmetrics)
-            d11 = dict(evbbatchmetrics)
-               
+            cursor = con.cursor() 
+            
+            x = {}
+            tables = ['isin','map_bcp','map_dataleaks','map_dotcom','map_gdpr',
+                      'map_isrisk', 'map_pas', 'map_pentest','map_remoteconnectivity',
+                      'map_servicenow', 'problem', 'problem_old']
+            for t in tables:
+                cursor.execute("select * from odi_test." + t)
+                colnames = [desc[0] for desc in cursor.description]
+                
+                if 'masterserviceid' in colnames:
+                    if 'service' in colnames:
+                        cursor.execute("select service,masterserviceid from odi_test." + t)
+                        l = cursor.fetchall()
+                    elif 'name' in colnames:
+                        cursor.execute("select name,masterserviceid from odi_test." + t)
+                        l = cursor.fetchall()
+                elif 'u_business_service' in colnames:
+                    if 'number' in colnames: 
+                        cursor.execute("select u_business_service,number from odi_test." + t)
+                        l = cursor.fetchall()
+                elif 'business_service' in colnames:
+                    if 'number' in colnames:
+                        cursor.execute("select business_service,number from odi_test." + t)
+                        l = cursor.fetchall()
+                
              
-            z = {**d1,**d2,**d3,**d4,**d5,**d6,**d7,**d8,**d9,**d10,**d11}
+                cleaningLists(l)
+                d = dict(l)
+                x[t] = d
+                
+            tables = ['isin','map_bcp','map_dataleaks','map_dotcom','map_gdpr',
+                      'map_isrisk', 'map_pas', 'map_pentest','map_remoteconnectivity',
+                      'map_servicenow', 'problem', 'problem_old']
+                
+            d1 = x.get('isin')
+            d2 = x.get('map_bcp')
+            d3 = x.get('map_dataleaks')
+            d4 = x.get('map_dotcom')
+            d5 = x.get('map_gdpr')
+            d6 = x.get('map_isrisk')
+            d7 = x.get('map_pas')
+            d8 = x.get('map_pentest')
+            d9 = x.get('map_remoteconnectivity')
+            d10 = x.get('map_servicenow')
+            d11 = x.get('problem')
+            d12 = x.get('problem_old')
+            
+            z = {**d1,**d2,**d3,**d4,**d5,**d6,**d7,**d8,**d9,**d10,**d11,**d12}
+            
+            #if 'Janes_Search_Report' in x.get('map_dotcom'): 
+            #    print('map_pentest -> DSMatch')
         
-        # groups together all the entries keys that have the same value
-        # i.e. have the same masterserviceid, so are the same products but under
-        # different names    
             sorted_dict = {}
             for k, v in z.items():
                 sorted_dict.setdefault(v, []).append(k)
                 
-        return z # perhaps consider storing this in some sort of table format
+            w = lookup(word,z)
+            
+            for a in w:
+                for key,value in x.items():
+                    if value == a:
+                        print(key)
+            
+            y = []
+            for a in w: # a is the word
+                for t in tables:
+                    if a in x.get(t):
+                        y.append(t + ' -> ' + a)
+                        
+            if len(y) == 0:
+                return 'no matches found'
+            else:
+                return y
+           
     
-    # for every element in z, we want to get the table name that it comes from
-            
-        #for key,val in sorted_dict.items():
-            #print(key,'=>',val)
-            
-        #print(z['Transportation - Business Segment'])
-        
-        # they specify a certain key
-        # find the value of that key
-        # use the value to find all the other keys that have the same value
-        
-        
-        
-        # lets you search for the word in a dictionary
+
         
