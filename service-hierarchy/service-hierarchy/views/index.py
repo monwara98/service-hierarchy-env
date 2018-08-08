7ff9e34b9836c4ec800d3648b8cd5315122df43e
Monwara Bibi <monwara.bibi@kcl.ac.uk>
2018-08-08 09:14:32 +0000

    search engine no longer case sensitive - must enter in all lower case

--- service-hierarchy/service-hierarchy/views/index.py
+++ service-hierarchy/service-hierarchy/views/index.py
@@ -21,12 +21,16 @@ else:
      
         if request.method == 'POST':
             name=request.form['name']
-     
+            flash("one")
             if form.validate(): # save the comment here
                 #testingSomething()
+                flash("two")
+                flash("this is the name: " + name)
                 if len(searchingDictionary(name)) > 0:
                     yList = searchingDictionary(name)
+                    flash("three")  
                 else:
+                    flash("four")
                     flash("search not found")
             else:
                 flash('All the form fields are required. ')
@@ -42,24 +46,42 @@ else:
             if l[1] == None:
                 list.remove(l)
                 
+    
+    def lower_dict(d):
+                new_dict = dict((k.lower(), v) for k, v in d.items())
+                return new_dict
+                
                 
                 
     def searchByValue(d,v):
+        #print("\nsearch by value method")
         try:
+         #   print("\nin the try block")
             listOfKeys = [key  for (key, value) in d.items() if value == v]
+          #  for l in listOfKeys:
+           #     print(l)
+            #print(len(listOfKeys))
         except Exception as e:
             listOfKeys = []
-        return listOfKeys
+            return listOfKeys
+        else:
+            return listOfKeys
     
     
     
     def lookup(word,d):
         if word in d:
-            try:
-                k = d[word] 
-            except Exception as e:
-                k = 0
+            #print("\nare we inside here?")
+            k = d[word]
+            #print("\nk is: " + k)
+            #for s in searchByValue(d,k):
+            #    print(s)
             return searchByValue(d,k)
+        #    try:
+        #        k = d[word] 
+        #    except Exception as e:
+        #        k = 0
+        #    return searchByValue(d,k)
         else:
             return "search not found"
         
@@ -78,6 +100,7 @@ else:
         table_names = []
         for r in row:
             table_names.append(r[0])
+        
         
         table_names = list(set(table_names))
         
@@ -141,6 +164,7 @@ else:
         except Exception as e:
             print("\ncould not connect to database\n")
         else:
+            print("\n hello??????")
             print("\nconnected to database\n")
         
             cursor = con.cursor() 
@@ -186,7 +210,12 @@ else:
              
                 cleaningLists(l)
                 d = dict(l)
-                x[t] = d
+                d1 = lower_dict(d)
+                x[t] = d1
+                
+            print(d)
+                
+            print("\ndid we leave the loop?")
                 
             #my_dict = {}
             #my_list = []
@@ -218,14 +247,25 @@ else:
             #d11 = x.get('problem')
             #d12 = x.get('problem_old')
             
+            
+            
+            
             z = {}
             for t in tables:
-                try:
-                    d = x.get(t)
-                except Exception as e:
-                    exit()
-                else:
-                    z = {**z,**d} # updayed the dictionary each time
+                d = x.get(t)
+                d1 = lower_dict(d)
+                z = {**z,**d1}
+                
+            print("\nsecond loop")
+                
+            
+            #for t in tables:
+            #    try:
+            #        d = x.get(t)
+            #    except Exception as e:
+            #        exit()
+            #    else:
+            #        z = {**z,**d} # updated the dictionary each time
             #print(z)
             
             #z = {**d1,**d2,**d3,**d4,**d5,**d6,**d7,**d8,**d9,**d10,**d11,**d12}
@@ -233,22 +273,29 @@ else:
             #z = {**d1}
             #z = {**z,**d2}
             #print(z)
-        
-            sorted_dict = {}
-            for k, v in z.items():
-                sorted_dict.setdefault(v, []).append(k)
+            
                 
             w = lookup(word,z)
-        
+            
+            print("")
+            for a in w:
+                print(a) 
+            print("")
             
             y = []
             for a in w: # a is the word
+                print("\nare we in here?")
                 for t in tables:
+                    #print(x.get(t))
                     if a in x.get(t):
+                        print("any difference?")
                         y.append([t.lower(),a.lower()])
+            print(len(y))
+            for a in y:
+                print(a)
                         
                          
-            
+                
             return y
    
         
