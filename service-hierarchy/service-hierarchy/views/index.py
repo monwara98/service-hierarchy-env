185bd9deba3f076dddb8a0e08fc35e14ece78f05
Monwara Bibi <monwara.bibi@kcl.ac.uk>
2018-08-08 09:22:14 +0000

    improved case [in]sensitivity issue

--- service-hierarchy/service-hierarchy/views/index.py
+++ service-hierarchy/service-hierarchy/views/index.py
@@ -20,17 +20,12 @@ else:
         yList = []
      
         if request.method == 'POST':
-            name=request.form['name']
-            flash("one")
+            name=(request.form['name']).lower()
             if form.validate(): # save the comment here
                 #testingSomething()
-                flash("two")
-                flash("this is the name: " + name)
                 if len(searchingDictionary(name)) > 0:
                     yList = searchingDictionary(name)
-                    flash("three")  
                 else:
-                    flash("four")
                     flash("search not found")
             else:
                 flash('All the form fields are required. ')
@@ -85,78 +80,7 @@ else:
         else:
             return "search not found"
         
-    def testingSomething():
-        try:
-            con = p.connect("dbname='odi' user='fmopex_test_ro' host='fmopex.cl19fspdhrve.eu-west-1.rds.amazonaws.com' password = 'admin'")
-        except Exception as e:
-            print("\ncould not connect to database\n")
-        else:
-            print("\nconnected to database\n")
-            
-        cursor = con.cursor()
-        cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
-        row = cursor.fetchall()
-        
-        table_names = []
-        for r in row:
-            table_names.append(r[0])
-        
-        
-        table_names = list(set(table_names))
-        
-        cursor.execute("select * from odi_test.problem")
-        l = cursor.fetchall()
-        colnames = [desc[0] for desc in cursor.description]
-        print(colnames)
-        
-        
-        
-        print(len(table_names))
-        
-        for t in table_names:
-            try:
-                cursor.execute("select * from odi_test." + t)
-                l = cursor.fetchall()
-            except Exception as e:
-                table_names.remove(t)
-            else:
-                colnames = [desc[0] for desc in cursor.description]
-                if (('business_service' in colnames) & ('number' in colnames)):
-                    pass
-                elif (('u_business_service' in colnames) & ('number' in colnames)):
-                    pass
-                elif (('masterserviceid' in colnames) & ('service' in colnames)):
-                    pass
-                elif (('masterserviceid' in colnames) & ('name' in colnames)):
-                    pass
-                else:
-                    table_names.remove(t)
-            
-        print(len(table_names))
-        
-        for t in table_names:
-            print(t)
-        
-       
-            
-        #tab = []
-            
-        #for r in row:
-        #    try:
-        #        cursor.execute("select * from odi_test." + r[0])
-        #        colnames = [desc[0] for desc in cursor.description]
-        #    except Exception as e:
-        #        return "hello"
-        #    else:
-        #        if (('masterserviceid' in colnames) & ('service' in colnames)) or (('name' in colnames) & ('masterserviceid' in colnames)):
-        #            if (('u_business_service' in colnames) & ('number' in colnames)) or (('business_service' in colnames) & ('number' in colnames)):
-        #                tab.append(r[0])
-                        
-                
-        #        print("hello")
-        #        for t in tab:
-        #            print(t)
-        
+   
     
     def searchingDictionary(word):
         try:
@@ -164,17 +88,9 @@ else:
         except Exception as e:
             print("\ncould not connect to database\n")
         else:
-            print("\n hello??????")
             print("\nconnected to database\n")
         
             cursor = con.cursor() 
-            
-            
-            #for r in row:
-                #print(r)
-                #cursor.execute("select * from odi_test." + r[0])
-                #li = cursor.fetchall()
-                #print(li)
             
             x = {}
             
@@ -213,39 +129,7 @@ else:
                 d1 = lower_dict(d)
                 x[t] = d1
                 
-            print(d)
                 
-            print("\ndid we leave the loop?")
-                
-            #my_dict = {}
-            #my_list = []
-            #my_dict = x.get('isin')
-            #my_dict = x.get('map_pentest')
-            #print(my_dict)
-            
-            #my_list.append(x.get('isin'))
-            #my_list.append(x.get('map_pentest'))
-            #print(my_list)
-            
-            #my_list = dict(my_list)
-            #print(my_list)
-                
-            # find a way to loop through the 'tables' list and add them all to
-            # the dictionary.
-            
-            # find a way to automate this 
-            #d1 = x.get('isin')
-            #d2 = x.get('map_bcp')
-            #d3 = x.get('map_dataleaks')
-            #d4 = x.get('map_dotcom')
-            #d5 = x.get('map_gdpr')
-            #d6 = x.get('map_isrisk')
-            #d7 = x.get('map_pas')
-            #d8 = x.get('map_pentest')
-            #d9 = x.get('map_remoteconnectivity')
-            #d10 = x.get('map_servicenow')
-            #d11 = x.get('problem')
-            #d12 = x.get('problem_old')
             
             
             
@@ -256,43 +140,16 @@ else:
                 d1 = lower_dict(d)
                 z = {**z,**d1}
                 
-            print("\nsecond loop")
-                
-            
-            #for t in tables:
-            #    try:
-            #        d = x.get(t)
-            #    except Exception as e:
-            #        exit()
-            #    else:
-            #        z = {**z,**d} # updated the dictionary each time
-            #print(z)
-            
-            #z = {**d1,**d2,**d3,**d4,**d5,**d6,**d7,**d8,**d9,**d10,**d11,**d12}
-            #print(z)
-            #z = {**d1}
-            #z = {**z,**d2}
-            #print(z)
             
                 
             w = lookup(word,z)
             
-            print("")
-            for a in w:
-                print(a) 
-            print("")
             
             y = []
             for a in w: # a is the word
-                print("\nare we in here?")
                 for t in tables:
-                    #print(x.get(t))
                     if a in x.get(t):
-                        print("any difference?")
-                        y.append([t.lower(),a.lower()])
-            print(len(y))
-            for a in y:
-                print(a)
+                        y.append([t,a])
                         
                          
                 
