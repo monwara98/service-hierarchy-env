3a298a7c5022215bc850eb4820e8312d365e982f
monwara98 <32258645+monwara98@users.noreply.github.com>
2018-08-10 12:36:04 +0100

    Update __main__.py

--- service-hierarchy/__main__.py
+++ service-hierarchy/__main__.py
@@ -1,7 +1,11 @@
 from service_hierarchy.app import app
 
 if __name__ == '__main__':
-    try:
+    try: 
+        # i was only using port 9999 because port 5000 (default port) seemed to already be in
+        # use on my computer. feel free to change it back to 5000, or just take off the port
+        # specifics in general. if you take off the port specifics you probably won't need a 
+        # try-catch block
         app.run(port=9999,debug=True)
     except Exception as e:
         app.run(debug=True)
